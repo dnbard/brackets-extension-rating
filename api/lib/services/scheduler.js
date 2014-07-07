9cb419efd2c306a2d72eb94679d05462d42385f4
Alex Bardanov <dnbard@gmail.com>
2014-07-07 22:35:37 +0300

    run registry worker every hour, #10

--- api/lib/services/scheduler.js
+++ api/lib/services/scheduler.js
@@ -13,5 +13,5 @@ exports.init = function(){
     var firstHerokuWorker = new ScheduleWorker({minute: 15}, pingPongHerokuHandler),
         secondHerokuWorker = new ScheduleWorker({minute: 45}, pingPongHerokuHandler);
 
-    var registryWorker = new ScheduleWorker({hour: 1}, registry.handler, true);
+    var registryWorker = new ScheduleWorker({minute: 1}, registry.handler, true);
 }
