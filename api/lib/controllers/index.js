6c7f237d504632ba6708dd180e5c480243a52495
Alex Bardanov <dnbard@gmail.com>
2014-07-22 15:53:47 +0300

    added jade for page templating

--- api/lib/controllers/index.js
+++ api/lib/controllers/index.js
@@ -1,8 +1,10 @@
-var stats = require('../services/stats');
+var stats = require('../services/stats'),
+    jade = require('jade');
 
 exports.default = function(req, res) {
-    var response = '<div>/ratings Requests last hour - ' + stats.get('ratings') + '</div>';
+    var response = jade.renderFile('lib/views/index.jade', {
+        ratings: stats.get('ratings')
+    });
 
-    return res.status(200)
-        .send('<div>Brackets Rating API Server - OK</div>' + response);
+    return res.status(200).send(response);
 };
