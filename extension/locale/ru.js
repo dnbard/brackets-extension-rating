1cfac1ffbe117e65c946e60ff6a923128cd87dc8
Dis Shishkov <dis@velocitymarkets.net>
2014-09-08 11:37:35 +0400

    Russian translation regards #84

--- extension/locale/ru.js
+++ extension/locale/ru.js
@@ -54,6 +54,10 @@ define(function (require, exports, module){
         versionTemplate: '<div>v ${version} от ${date} - ${downloads} загрузок</div>',
 
         //... and ## more versions
-        moreVersionsTemplate: '<div>... и ${count} ещё версий</div>'
+        moreVersionsTemplate: '<div>... и ${count} ещё версий</div>',
+        
+        //0.5.4
+        colorDark: 'тёмная',
+        colorLight: 'светлая'
     }
 });
