0d176c93d1ec775217e3148a73b7dde9ca5fef17
Alex Bardanov <dnbard@gmail.com>
2015-07-12 11:43:19 +0300

    debug: log incoming /tick requests

--- api.online/lib/controllers/api.js
+++ api.online/lib/controllers/api.js
@@ -15,6 +15,8 @@ function tick(req, res){
         userId = req.params.user,
         user, list;
 
+    console.log('Tick request from %s', ipAddr);
+
     if (userId && usersCache.check(userId)){
         user = userId;
     } else {
